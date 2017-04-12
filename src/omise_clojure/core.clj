1eeacb751bbbb60cb4c6f4ad03becaa1b6b0911f
Weerasak Chongnguluam <singpor@gmail.com>
2017-04-12 23:50:01 +0700

    HTTP POST with content-type json

--- src/omise_clojure/core.clj
+++ src/omise_clojure/core.clj
@@ -1,11 +1,14 @@
 (ns omise-clojure.core)
 
 (require '[clj-http.client :as client])
+(require 'cheshire.core)
 
 (defn create-token
   [endpoint pkey params]
-  (client/post endpoint
-               {
-                :as :json
-                :basic-auth [pkey ""]
-                :form-params params}))
+  (let [body (cheshire.core/encode params)]
+    (client/post endpoint
+                 {
+                  :as :json
+                  :basic-auth [pkey ""]
+                  :content-type :json
+                  :body body})))
